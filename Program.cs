f662b36c551a6ea758b6ac80dc411c449472943e
ThreeDokNait <nikitaaleksandrovichshiyatov@gmail.com>
2023-08-05 21:15:57 +0300

    Спасибо за внимание! `

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿Console.Clear();
+
+// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+// Console.WriteLine("get number 1 for me");
+// double number = Convert.ToDouble(Console.ReadLine());
+
+// Console.WriteLine("get number 2 for me");
+// double number2 = Convert.ToDouble(Console.ReadLine());
+// void Por()
+// {
+//     Console.WriteLine(Math.Pow (number, number2));
+// }
+// Por();
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// Console.WriteLine("get number for me");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int Sum()
+// {
+// int sum =0;
+
+// while (num>0)
+// {
+//     sum = sum + num%10;
+//     num/= 10;
+// }
+// return sum;
+
+// }
+// Console.WriteLine(Sum());
+
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+// int[] array = new int[8];
+
+// void PrintArray()
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(0, 255);
+//         Console.Write(array[i]+" ");
+//     }
+// }
+// PrintArray();
